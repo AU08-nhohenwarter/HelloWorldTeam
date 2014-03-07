43ad6a9946f895c0a0229703cc3196c4077b2a8e
Keilholz Inga <ikeilholz@student.tgm.ac.at>
2014-03-07 12:13:43 +0100

    h

--- HelloWorldTeam_nhohenwarter/src/hohenwarter/HelloWorld.java
+++ HelloWorldTeam_nhohenwarter/src/hohenwarter/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 	}
 	
 	public static void gruesseKeilholz(){
-		System.out.println("Schöne Grüße von Inga! :)");
+		System.out.println("Schöne Grüße von Inga! :3");
 	}
 	
 	public static void gruesseBrinnich(){
